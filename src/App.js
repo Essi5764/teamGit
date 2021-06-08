9ea984fb3ed6c43c78b00061d0bcc10f4c0d9c5b
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-08 16:40:24 -0400

    Rough layout of header, footer, and sidebar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css';
 
+import Header from './components/site/Headers'
+import Footer from './components/site/Footer';
+import Sidebar from './components/site/Sidebar';
+import {
+  BrowserRouter as Router
+} from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-     Test 
+    <div className='App'>
+    <Header />
+    <Router>
+      <Sidebar />
+    </Router>
+    <Footer />
     </div>
   );
 }
---
+++ src/components/site/Headers.js
@@ -0,0 +1,17 @@
+import {
+    Navbar,
+    NavItem,
+} from 'reactstrap';
+
+
+const Header = () => {
+    return(
+        <header>
+            <h1 className='header'>
+                What's happening in:</h1>
+            
+        </header>
+    );
+};
+
+export default Header;
