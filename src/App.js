c4b52a58390bd28d57001b60419f4380786ce465
mrenee73 <michelecountryman1@gmail.com>
2021-06-08 22:54:43 -0400

    added npx create app geolocation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import React, { useEffect, useState } from 'react'; //this replace line one? 
+
 import 'bootstrap/dist/css/bootstrap.css'
 
 import './App.css';
-
 
 import Header from './components/site/Headers'
 import Footer from './components/site/Footer';
@@ -23,6 +24,48 @@ function App() {
   );
 }
 
+const useGeoLocation = () => {
+  const [location, setLocation] = useState({
+      loaded: false,
+      coordinates: {lat: "", lng: ""}
+  });
+  
+  const onSuccess = location => {
+      setLocation({
+          loaded: true,
+          coordinates: {
+              lat: location.coords.latitude,
+              lng: location.coords.longitude,
+          }, 
+      });
+  };
 
+  const onError = error => {
+      setLocation({
+          loaded: true,
+          error,
+      });
+  };
 
+  useEffect(()  => {
+      if( !("geolocation" in navigator)){
+          onError({
+              code: 0,
+              message: "Geolocation not supported",
+          });
+          // setLocation((state) => ({
+          //     ...state,
+          //     loaded: true,
+          //     error: {
+          //         code: 0,
+          //         message: "Geolocation not supported"
+          //     },
+          // }));
+      }
+
+      navigator.geolocation.getCurrentPosition(onSuccess, onError)
+  }, [])
+  return location;
+}
+export default useGeoLocation
 export default App;
