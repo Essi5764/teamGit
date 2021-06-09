26cc230eb3b15095ae9444fcd03e2e930739d6b9
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-08 20:10:40 -0400

    Routing for Navbar and placeholder folder/files added for api injections

---
+++ src/components/apis/Nasa.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Nasa = () => {
+
+    return(
+<div className='main'>
+    <div className='mainDiv'>
+        <p>Placeholder for Nasa.</p>
+    </div>
+</div>
+    )
+};
+
+export default Nasa;
--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -4,28 +4,34 @@ import {
     Switch
 } from 'react-router-dom';
 import {Button} from 'reactstrap';
+import Nasa from '../apis/Nasa';
+import Weather from '../apis/Weather';
+import Tickets from '../apis/Tickets';
 
 
 const Sidebar = () => {
     return(
         <div className='sidebar'>
             <div className='sidebar-list-styling'>
-                <Button>Check Satellite Image</Button>
+                <Button><Link to='/nasa'>Check Satellite Image</Link></Button>
                 <br/>
                 <br/>
                 <br/>
-                <Button>Check Weather</Button>
+                <Button><Link to='/weather'>Check Local Weather</Link></Button>
                 <br/>
                 <br/>
                 <br/>
-                <Button>Check Local Events</Button>
+                <Button><Link to='/tickets'>Check Local Events</Link></Button>
                 <br/>
                 <br/>
                 <br/>
             </div>
             <div className='sidebar-route'>
                 <Switch>
-                 
+                 <Route exact path='/nasa'><Nasa /></Route>
+                 <Route exact path='/weather'><Weather /></Route>
+                 <Route exact path='/tickets'><Tickets /></Route>
+
                 </Switch>
             </div>
         </div>
