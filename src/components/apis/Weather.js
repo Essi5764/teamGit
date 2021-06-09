addd6ace209f0409ac746b97da69211d3db75fed
Essi Monneus <essimonneus@gmail.com>
2021-06-09 10:58:42 -0400

    update

--- src/components/apis/Weather.js
+++ src/components/apis/Weather.js
@@ -1,66 +1,37 @@
-// const Weather = () => {
+import React, {useState, useEffect} from 'react'
 
-//     return(
-// <div className='main'>
-//     <div className='mainDiv'>
-//         <p>Placeholder for Weather.</p>
-//     </div>
-// </div>
-//     )
-// };
 
-// export default Weather;
 
-import React, { useState, useEffect } from 'react';
-// Helpers
-import { formatDate } from './helpers';
-// API
-import { API_URL } from './weather';
-// Components
-import WeatherData from './components/WeatherData';
-import Info from './components/Info';
-import Unit from './components/Unit';
-import Previous from './components/Previous';
-// Img
-// import BGImage from './img/mars.jpg';
-// Styles
-import {
-  AppWrapper,
-  GlobalStyle,
-  MarsWeather,
-  InfoWrapper,
-} from './App.styles';
+const Weather = () => {
+    const [locationImage, setLocationImage] = useState('')
 
-const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [weather, setWeather] = useState([]);
-  const [selectedSol, setSelectedSol] = useState();
-  const [metric, setMetric] = useState(true);
-  const [previous, setPrevious] = useState(false);
-  console.log(weather);
+    const fetchWeatherData = (props) => {
+        // let [month, date, year]    = new Date().toLocaleDateString("en-US").split("/")
+        // console.log(year, month, date);
+        let url =`'https://api.openweathermap.org/data/2.5/weather?lat=${localStorage.longitude}&lon=${localStorage.longitude}&appid=568cce6843d72e9266b7b083cca8b163'`
+        console.log(url);
+        fetch(url,{
+            method: 'GET'})
+        .then(results => results.json())
+        .then(json => console.log(json))
+        
+        
 
-  useEffect(() => {
-    const fetchFromAPI = async () => {
-      const weather = await (await fetch(API_URL)).json();
-      const marsWeather = weather.sol_keys.map((solKey) => {
-        return {
-          sol: solKey,
-          maxTemp: weather[solKey].AT?.mx || 'No data',
-          minTemp: weather[solKey].AT?.mn || 'No data',
-          windSpeed: Math.round(weather[solKey].HWS?.av || 0),
-          windDirectionDegrees:
-            weather[solKey].WD?.most_common?.compass_degrees || 0,
-          date: formatDate(new Date(weather[solKey].First_UTC)),
-        };
-      });
-      setWeather(marsWeather);
-      setSelectedSol(marsWeather.length - 1);
-      setLoading(false);
-    };
+    }
+    
+    useEffect(() => {
+        fetchWeatherData();
+    }, [])
 
-    fetchFromAPI();
-  }, []);
+    return(
+<div className='main'>
+    <div className='mainDiv'>
+        <p>Placeholder for Nasa.</p>
+    </div>
+</div>
+    )
+};
 
-  return (
-   
-export default App;
+export default Nasa;
+
+
