456f14ad1199b52e8f5d1ec2a86df5c3cf892e9a
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-09 13:01:54 -0400

    final push

--- src/components/apis/Weather.js
+++ src/components/apis/Weather.js
@@ -4,26 +4,26 @@ import React, {useState, useEffect} from 'react'
 
 
 const Weather = () => {
-    const [setLocationTemeprature] = useState('')
+    // const [setLocationTemeprature] = useState('')
 
-    const fetchWeatherData = (props) => {
+    // const fetchWeatherData = (props) => {
         
-        let url = 'https:api.openweathermap.org/data/2.5/weather?lat=${localStorage.latitude}&lon=${localStorage.longitude}&appid=568cce6843d72e9266b7b083cca8b163'
+    //     let url = 'https:api.openweathermap.org/data/2.5/weather?lat=${localStorage.latitude}&lon=${localStorage.longitude}&appid=568cce6843d72e9266b7b083cca8b163'
 
-        console.log(url);
-        fetch(url,{
-            method: 'GET'})
-            .then(results =>{
-                setLocationTemeprature(results.url)
-            })
+    //     console.log(url);
+    //     fetch(url,{
+    //         method: 'GET'})
+    //         .then(results =>{
+    //             setLocationTemeprature(results.url)
+    //         })
         
         
 
-    }
+    // }
     
-    useEffect(() => {
-        fetchWeatherData();
-    }, [])
+    // useEffect(() => {
+    //     fetchWeatherData();
+    // }, [])
 
     return(
 <div className='main'>
